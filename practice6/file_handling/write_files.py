ae485f8281bf8dabd7101838198a5440f3a1ccde
deuterium <zglnrn12@gmail.com>
2026-03-16 20:34:47 +0500

    write files

---
+++ practice6/file_handling/write_files.py
@@ -0,0 +1,8 @@
+with open("transcript.txt", "w") as f:
+    f.write("calc1 91\n")
+    f.write("ict 95\n")
+    f.write("soc pol 81\n")
+
+with open("transcript.txt", "a") as f:
+    f.write("linal 75\n")
+    f.write("pp1 86")
