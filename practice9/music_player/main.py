e07b2beeb0e44aac79ada01510537f886a0a8dd5
deuterium <zglnrn12@gmail.com>
2026-04-11 14:21:56 +0500

    music player

--- practice9/music_player/main.py
+++ practice9/music_player/main.py
@@ -4,14 +4,14 @@ import os
 pygame.init()
 pygame.mixer.init()
 
-screen = pygame.display.set_mode((700, 500))
+screen = pygame.display.set_mode((800, 500))
 pygame.display.set_caption("Music Player")
 
 font = pygame.font.Font(None, 32)
 small_font = pygame.font.Font(None, 26)
 clock = pygame.time.Clock()
 
-music_directory = "/home/deuteruim/Desktop/code/pp2/practice9/player/music"
+music_directory = "/home/deuteruim/Desktop/code/pp2/practice9/music_player/music"
 
 music_files = []
 for file in os.listdir(music_directory):
@@ -66,39 +66,39 @@ while running:
             elif event.key == pygame.K_q:
                 running = False
 
-    screen.fill((255, 255, 255))
+    screen.fill((175, 238, 238)) #pale turqoise
 
-    title = font.render("Music Player", True, (0, 0, 0))
-    screen.blit(title, (270, 20))
+    title = font.render("Music Player", True, (0, 0, 128))
+    screen.blit(title, (320, 50))
 
     instructions = small_font.render(
         "SPACE - Play/Pause   LEFT - Previous   RIGHT - Next   Q - Quit",
         True,
-        (0, 0, 0)
+        (0, 0, 128)
     )
-    screen.blit(instructions, (30, 60))
+    screen.blit(instructions, (150, 100))
 
     if len(music_files) > 0:
-        current_text = font.render("Current track:", True, (0, 0, 0))
-        screen.blit(current_text, (50, 140))
+        current_text = font.render("Current track:", True, (0, 0, 128))
+        screen.blit(current_text, (50, 200))
 
-        track_text = small_font.render(music_files[current_track], True, (0, 0, 255))
-        screen.blit(track_text, (50, 180))
+        track_text = small_font.render(music_files[current_track], True, (0, 0, 0))
+        screen.blit(track_text, (50, 240))
 
         list_title = font.render("Playlist:", True, (0, 0, 0))
-        screen.blit(list_title, (420, 140))
+        screen.blit(list_title, (420, 200))
 
-        y = 190
+        y = 240
         for i in range(len(music_files)):
             if i == current_track:
-                text = small_font.render("> " + music_files[i], True, (255, 0, 0))
+                text = small_font.render("> " + music_files[i], True, (65, 105, 225))
             else:
                 text = small_font.render(music_files[i], True, (0, 0, 0))
             screen.blit(text, (420, y))
             y += 35
     else:
         no_music = font.render("No music files found", True, (255, 0, 0))
-        screen.blit(no_music, (180, 220))
+        screen.blit(no_music, (180, 240))
 
     pygame.display.flip()
     clock.tick(30)
