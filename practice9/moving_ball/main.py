0b6b683863837034d5d58b20f9504c25c7890e99
deuterium <zglnrn12@gmail.com>
2026-04-18 10:15:38 +0500

    ch

--- practice9/moving_ball/main.py
+++ practice9/moving_ball/main.py
@@ -8,27 +8,20 @@ screen = pygame.display.set_mode((800, 500))
 ball = Ball()
 clock = pygame.time.Clock()
 
-while True:
+done = True
+while done:
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
+            done = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 pygame.quit()
-                sys.exit()
-
             if event.key == pygame.K_UP:
                 ball.move_up()
-
             if event.key == pygame.K_DOWN:
                 ball.move_down(500)
-
             if event.key == pygame.K_LEFT:
                 ball.move_left()
-
             if event.key == pygame.K_RIGHT:
                 ball.move_right(800)
 
@@ -36,3 +29,5 @@ while True:
     ball.draw(screen)
     pygame.display.flip()
     clock.tick(60)
+    
+pygame.quit()
